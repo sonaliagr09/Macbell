c358616b79a3595cbafd157bf98b8b8711d15efa
Revati Mahajan <mahajan21revati@gmail.com>
2020-07-02 23:55:17 +0530

    register form

--- FrontEnd/src/app/register/register.component.ts
+++ FrontEnd/src/app/register/register.component.ts
@@ -12,6 +12,17 @@ export class RegisterComponent implements OnInit {
   form: FormGroup;
     loading = false;
     submitted = false;
+    step1 = true;
+    step2 = false;
+    profileTypes = {
+        'student': ['intership', 'job seeker', 'freelancing work', 'startup idea', 'startup', 'join startup as a cofounder', 'join startup as team member'],
+        'entrepreneur': ['startup idea', 'startup', 'join startup as a cofounder', 'join startup as team member'],
+        'student entrepreneur': ['intership', 'job seeker', 'freelancing work', 'startup idea', 'startup', 'join startup as a cofounder', 'join startup as team member'],
+        'businessman': ['company', 'firm' , 'franchisee' , 'Distributors', 'wholesalers' , 'investor'],
+        'investor' : ['investor'],
+        'freelancer' : ['freelancer']
+    };
+    profiles = [];
 
     constructor(
         private formBuilder: FormBuilder,
@@ -20,21 +31,28 @@ export class RegisterComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+        // console.log(this.profileTypes.keys());
+        console.log(typeof(this.profileTypes));
         this.form = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
             email: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]],
+            category: ['', [Validators.required]],
             profileType: ['', [Validators.required]],
-            address: ['', [Validators.required]],
-            contact: ['', [Validators.required, Validators.minLength(9)]],
-            country: ['', [Validators.required]],
-            city: ['', [Validators.required]],
+            // address: ['', [Validators.required]],
+            // contact: ['', [Validators.required, Validators.minLength(9)]],
+            // country: ['', [Validators.required]],
+            // city: ['', [Validators.required]],
         });
     }
 
     // convenience getter for easy access to form fields
     get f() { return this.form.controls; }
+
+    changeProfileType (val) {
+        this.profiles = this.profileTypes[val];
+    }
 
     onSubmit() {
         this.submitted = true;
@@ -42,9 +60,13 @@ export class RegisterComponent implements OnInit {
         
 
         if (this.form.invalid) {
+            console.log("form invalid");
             return;
         }
-        console.log(console.log(this.form.value))
+        else {
+            console.log(console.log(this.form.value))
+
+        }
         
         // this.loading = true;
     }
